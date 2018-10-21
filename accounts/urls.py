1d8eb9f80bdb7d014d76d49880c6c0b8f8379196
芝 黎 <yezetong@163.com>
2018-10-21 15:06:04 +0800

    base template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', views.home),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'))
     # path('', views.IndexView.as_view(), name='index'),
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
